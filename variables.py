72e0cd1fc38e1293c1a4c74002c2596a8fe363ad
Ritterl27 <ritterl27@chcstudent.com>
2025-09-04 10:48:52 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,25 @@
+num = 5
+"""
+this creates a variable, named num, and its attribute is the number 5
+"""
+
+"""
+print (<variable name)
+"""
+print(num)
+x=10
+print(x)
+"""
+now we can do 4 basic function arithmatic
+adding +
+subtracting -
+multiplying *
+dividing /
+"""
+print(num + x)
+print(x - num)
+print(num * x)
+print(x / num)
+
+sum = num + x
+print(sum)
