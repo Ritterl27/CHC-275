01b58783776ceb9d057400522f3dfcdfa9bc43de
MurrayC27 <MurrayC27@chcstudent.com>
2025-10-07 10:16:04 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -12,15 +12,35 @@ while check == False:
         print("6.remove account")
         x = input("what # action would you like to do? ")
         if x == ("3"):
-            print(accounts,balances)
-        if x == ("5"):
+            accountname4 = input("what is your account name? ")
+            index = accounts.index(accountname4)
+            print(balances[index])
+        elif x == ("5"):
             accountname = input("what is your account name? ")
             accounts.append(accountname)
             balances.append(0)
             print("your account has been added")
         elif x == ("6"):
             accountname2 = input("what is your account name? ")
-            accounts.remove(accountname2)
-            print("your account has been removed, your money is lost :( ")
-        
+            index = accounts.index(accountname2)
+            accounts.pop(index)
+            balances.pop(index)
+            print("your account and balance has been removed. ")
+        elif x == ("1"):
+            accountname3 = input("what is your account name? ")
+            index = accounts.index(accountname3)
+            x = input("how much would you like to add? ")
+            x = int(x)
+            balances[index] = balances[index] + x
+            y = balances[index]
+            print(f"your new balance is {y} ")
+        elif x ==("2"):
+            accountname5 = input("what is your account name? ")
+            index = accounts.index(accountname5)
+            x = input("how much would you like to withdraw? ")
+            x = int(x)
+            balances[index] = balances[index] - x
+            y = balances[index]
+            print(f"your new balance is {y} ")
+            
             
