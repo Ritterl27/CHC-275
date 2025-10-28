33d827ec321d24a4819de99422654eae6acd711c
Ritterl27 <ritterl27@chcstudent.com>
2025-10-28 12:07:36 -0400

    Update calc3.py

--- calc3.py
+++ calc3.py
@@ -1,32 +1,33 @@
 check = False
+print("Welcome to the calculator")
 while check == False:
     option = input("what operation do you want to do? (type quit to exit): ").strip().lower()
     try:
         if option == "addition":
-            x = input("what is x? ")
+            x = input("what is x? ").strip()
             x = int(x)
-            y = input("what is y? ")
+            y = input("what is y? ").strip()
             y = int(y)
             sum = x + y
             print(f"the sum of {x} and {y} is {sum}")
         elif option == "subtraction":
-             x = input("what is x? ")
+             x = input("what is x? ").strip()
              x = int(x)
-             y = input("what is y? ")
+             y = input("what is y? ").strip()
              y = int(y)
              difference = x - y
              print(f"the difference of {x} and {y} is {difference}")
         elif option == "multiplication":
-            x = input("what is x? ")
+            x = input("what is x? ").strip()
             x = int(x)
-            y = input("what is y? ")
+            y = input("what is y? ").strip()
             y = int(y)
             product = x * y
             print(f"the product of {x} and {y} is {product}")
         elif option == "division":
-             x = input("what is x? ")
+             x = input("what is x? ").strip()
              x = int(x)
-             y = input("what is y? ")
+             y = input("what is y? ").strip()
              y = int(y)
              try:
                 quotiant = x / y
@@ -34,6 +35,7 @@ while check == False:
              except ZeroDivisionError:
                  print("Error: cannot divide by zero")
     except ValueError:
-        print("that is not a number")
+        print("that is not a numerical value")
     if option == "quit":
         check = True
+        print("Thanks for using the calculator")
