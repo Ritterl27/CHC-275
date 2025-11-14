84ee211a1be019034e81079267e77cf26d1f5095
MurrayC27 <MurrayC27@chcstudent.com>
2025-11-14 09:22:27 -0500

    commit

--- StockMarketEval.py
+++ StockMarketEval.py
@@ -13,13 +13,45 @@ for i in range(len(msft)):
     amzn[i] = int(amzn[i])
     nvda[i] = int(nvda[i])
 
-print(msft)
 msftavg1 = sum(msft)/len(msft)
-print(msftavg1)
+amznavg1 = sum(amzn)/len(amzn)
+nvdaavg1 = sum(nvda)/len(nvda)
 
 
 
+file = open("day2.txt","r")
+buffer = file.readlines()
+file.close()
+msft = buffer[0].split(",")
+amzn = buffer[1].split(",")
+nvda = buffer[2].split(",")
+msft.pop(0)
+amzn.pop(0)
+nvda.pop(0)
 
+for i in range(len(msft)):
+    msft[i] = int(msft[i])
+    amzn[i] = int(amzn[i])
+    nvda[i] = int(nvda[i])
+
+msftavg2 = sum(msft)/len(msft)
+amznavg2 = sum(amzn)/len(amzn)
+nvdaavg2 = sum(nvda)/len(nvda)
+buys = []
+if msftavg2 > msftavg1:
+    buys.append("msft")
+if amznavg2 > amznavg1:
+    buys.append("amzn")
+if nvdaavg2 > nvdaavg1:
+    buys.append("nvda")
+line0 = f"msftavg1: {msftavg1}, msftavg2: {msftavg2}\n"
+line1 = f"amznavg1: {amznavg1}, amznavg2: {amznavg2}\n"
+line2 = f"nvdaavg1: {nvdaavg1}, nvdaavg2: {nvdaavg2}\n"
+line3 = f"buys: {buys}"
+buffer = [line0,line1,line2,line3]
+file = open("report.txt","w")
+file.writelines(buffer)
+file.close()
 
 
 
